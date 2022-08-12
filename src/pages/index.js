ac07cea5bce21baa746fefdfcb9ed7728333dc37
Sabik22 <sabikali2202@gmail.com>
2022-08-12 11:52:40 +0530

    sabikali

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,110 @@
-import * as React from "react"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import * as React from 'react'
+import './mystyle.css';
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <body>
+      <main>
+      <title>React</title>
+      <div className="color"></div>
+      <div className="absolute">
+      <div className="responsive1">
+        <div className="container1">
+          <div className="clearfix">
+          <svg className="svgimg" xmlns="http://www.w3.org/2000/svg" width="16rem" height="20rem" viewBox="0 0 141.789 154.75">
+  <g id="Group_6159" data-name="Group 6159" transform="translate(-1063 -2587)">
+    <circle id="Ellipse_1121" data-name="Ellipse 1121" cx="70.895" cy="70.895" r="70.895" transform="translate(1063 2587)" fill="#d1d1d1" opacity="0.331"/>
+    <g id="Group_6158" data-name="Group 6158" transform="translate(1088.089 2609.538)">
+      <path id="security-camera_1_" data-name="security-camera (1)" d="M30.408,43.921a.965.965,0,0,0-1.315.348l-2.48-1.432.216-.374a2.559,2.559,0,0,0-.936-3.491L7.806,28.53a2.559,2.559,0,0,0-3.491.935L.335,36.36a2.559,2.559,0,0,0,.935,3.491l8.56,4.942v5.339h-4a4.594,4.594,0,0,0-4.483-3.621.966.966,0,0,0-.966.966v7.241a.966.966,0,0,0,.966.966,4.594,4.594,0,0,0,4.483-3.621H10.8a.966.966,0,0,0,.966-.966V45.908l7.6,4.385a2.559,2.559,0,0,0,3.491-.935l.129-.224,2.51,1.449a.965.965,0,0,0,1.769.727l.355-.615h0l3.149-5.454A.966.966,0,0,0,30.408,43.921ZM2.316,53.571V48.624a2.655,2.655,0,0,1,0,4.947ZM20.63,48.7a.608.608,0,0,1-.308-.082L2.236,38.178a.633.633,0,0,1-.229-.853l3.98-6.894a.63.63,0,0,1,.546-.311.608.608,0,0,1,.308.082L24.927,40.645a.633.633,0,0,1,.229.853l-.7,1.209h0l-2.668,4.621h0l-.612,1.06a.63.63,0,0,1-.546.311Zm5.017-4.193,2.48,1.432-1.7,2.951-2.48-1.432ZM11.852,36.26a.965.965,0,0,1-1.319.353L7.7,34.977A.966.966,0,0,1,8.664,33.3L11.5,34.942A.965.965,0,0,1,11.852,36.26Z" transform="translate(7.079 -24.071)" fill="#410b5f"/>
+      <line id="Line_342" data-name="Line 342" y2="99.646" transform="translate(8.248 22.946)" fill="none" stroke="#3b125b" stroke-width="3"/>
+      <g id="flat" transform="translate(0 0)">
+        <path id="Path_18771" data-name="Path 18771" d="M1584.562-12.832h6.93L1596.12,8l-15.827,8.287-.917-6.417a60.463,60.463,0,0,1,.215-18.488,5.04,5.04,0,0,1,4.971-4.211Z" transform="translate(-1519.821 39.708)" fill="#ff9831"/>
+        <path id="Path_18772" data-name="Path 18772" d="M1614.213,92.127h9.519l4.479,17.918h8.959L1634.776,88.5l-4.325-5.329Z" transform="translate(-1545.342 -29.415)" fill="#410b5f"/>
+        <path id="Path_18773" data-name="Path 18773" d="M1376.213,163.168H1443.4v50.393a3.359,3.359,0,0,1-3.359,3.36h-60.472a3.359,3.359,0,0,1-3.359-3.36V163.168Z" transform="translate(-1373.973 -87.018)" fill="#ffd332"/>
+        <path id="Path_18774" data-name="Path 18774" d="M1371.012,115.168h66.071a2.8,2.8,0,0,1,2.8,2.8v10.639h-71.67V117.968A2.8,2.8,0,0,1,1371.012,115.168Z" transform="translate(-1368.213 -52.456)" fill="#ffd332"/>
+        <path id="Path_18775" data-name="Path 18775" d="M1600.213-60.832v13.438h6.719v-4.479h3.36a1.12,1.12,0,0,0,1.12-1.12v-7.839Z" transform="translate(-1535.262 74.27)" fill="#e6a98d"/>
+        <path id="Path_18776" data-name="Path 18776" d="M1607.779-106.37l-2.462-2.462-1.451,1.451a18.145,18.145,0,0,1-7.091,4.383A2.8,2.8,0,0,0,1595-99.457h0l1.354,4.063h11.2l1.512-3.024a6.887,6.887,0,0,0,.728-3.08A6.886,6.886,0,0,0,1607.779-106.37Z" transform="translate(-1531.407 108.832)" fill="#1c1c1c"/>
+        <path id="Path_18777" data-name="Path 18777" d="M1680.213-4.337l3.36-2.24a4.038,4.038,0,0,1,5.6,1.12,4.038,4.038,0,0,1-1.12,5.6l-3.36,2.24Z" transform="translate(-1592.865 35.693)" fill="#e6a98d"/>
+        <path id="Path_18778" data-name="Path 18778" d="M1625.65-1.633l-14.125,8.1a6.857,6.857,0,0,1-9.835-3.826l-3.253-9.758a4.339,4.339,0,0,1,2.745-5.489,4.345,4.345,0,0,1,1.37-.223,4.339,4.339,0,0,1,4.029,2.728l3.39,8.471,11.2-6.719Z" transform="translate(-1533.822 39.708)" fill="#ff9831"/>
+        <path id="Path_18779" data-name="Path 18779" d="M1584.213,60.464l.2.593a8.959,8.959,0,0,0,8.5,6.126h.387a8.96,8.96,0,0,0,4.006-.946l11.547-5.773,15.678,11.2,6.719-7.26L1612.9,50.171a7.839,7.839,0,0,0-8.559-.688Z" transform="translate(-1523.741 -4.471)" fill="#410b5f"/>
+        <g id="Group_6157" data-name="Group 6157" transform="translate(50.555 59.931)">
+          <path id="Path_18780" data-name="Path 18780" d="M1664.213,179.168v6.719l11.2,4.479,2.24-6.719-4.479-4.479Z" transform="translate(-1631.899 -158.47)" fill="#e2e2e2"/>
+          <path id="Path_18781" data-name="Path 18781" d="M1734.932,105.235l6.719.541,2.24,2.24-8.959,9.881-6.719-5.4Z" transform="translate(-1677.981 -105.235)" fill="#e2e2e2"/>
+          <path id="Path_18782" data-name="Path 18782" d="M18.355,0A18.355,18.355,0,1,1,0,18.355,18.355,18.355,0,0,1,18.355,0Z" transform="translate(0 33.622)" fill="#e2e2e2"/>
+        </g>
+        <path id="Path_18783" data-name="Path 18783" d="M1376.213,163.168H1443.4v4.479h-67.191Z" transform="translate(-1373.973 -87.018)" fill="#d18d28"/>
+        <path id="Path_18784" data-name="Path 18784" d="M1674.019,3.218l4.427,6.907,1.787-1.024-4.365-7.025Z" transform="translate(-1588.405 28.974)" fill="#422608"/>
+        <path id="Path_2120" data-name="Path 2120" d="M1555.354,232.094a16.653,16.653,0,0,1,21.231-1.933l-2.135.219.226,2.2,5.748-.59-.589-5.748-2.2.226.191,1.864a18.867,18.867,0,0,0-26.351,26.067l1.833-1.235-.075-.114A16.657,16.657,0,0,1,1555.354,232.094Z" transform="translate(-1497.861 -131.547)" fill="#410b5f" stroke="#410b5f" stroke-width="2"/>
+        <path id="Path_2121" data-name="Path 2121" d="M1597.1,254.758l-1.833,1.235c.026.038.05.076.075.114A16.654,16.654,0,0,1,1571.987,279l2.135-.219-.226-2.2-5.748.589.589,5.748,2.2-.226-.191-1.864a18.867,18.867,0,0,0,26.351-26.067Z" transform="translate(-1512.175 -152.966)" fill="#410b5f" stroke="#410b5f" stroke-width="2"/>
+        <path id="Path_2122" data-name="Path 2122" d="M1572.479,285.132l1.431-1.323a3.031,3.031,0,0,0,1.29-2.407c0-1.41-1.2-2.309-2.949-2.309a3.479,3.479,0,0,0-3.144,1.583l1.593.889a1.55,1.55,0,0,1,1.355-.737c.651,0,.978.293.978.781,0,.314-.085.661-.694,1.225l-2.84,2.656v1.345h5.907v-1.7Z" transform="translate(-1512.864 -170.489)" fill="#410b5f"/>
+        <path id="Path_2123" data-name="Path 2123" d="M1600.265,285.777h1.127v-1.7h-1.127v-1.288h-2.027v1.29h-1.54l3.09-4.434h-2.222l-3.4,4.738v1.4h4.011v1.453h2.092Z" transform="translate(-1530.904 -170.885)" fill="#410b5f"/>
+        <path id="Path_2124" data-name="Path 2124" d="M1622.251,284.349l3.578-10.212H1624l-3.578,10.212Z" transform="translate(-1549.811 -166.92)" fill="#410b5f"/>
+        <path id="Path_2125" data-name="Path 2125" d="M1641.53,282.513v-1.172h1.995l-2.559,5.887h2.342l2.677-6.245v-1.342H1639.7v2.873Z" transform="translate(-1563.691 -170.883)" fill="#410b5f"/>
+      </g>
+    </g>
+  </g>
+</svg>
+<h2>DUMMY TEXT</h2>
+          <p>Lorem Ipsum has been the industry's standard.</p>
+</div>
+        </div>
+      </div>
+      <div className="responsive2">
+        <div className="container2"> <h3>Self Storage</h3><p className="p1">Lorem Ipsum</p>
+        <svg id="Group_6241" data-name="Group 6241" xmlns="http://www.w3.org/2000/svg" width="15rem" height="12rem" viewBox="0 0 112.734 75.157">
+  <rect id="Rectangle_282" data-name="Rectangle 282" width="37.578" height="37.578" transform="translate(18.788)" fill="#ff9831"/>
+  <rect id="Rectangle_283" data-name="Rectangle 283" width="37.578" height="37.578" transform="translate(56.367 37.578)" fill="#ff9831"/>
+  <g id="Group_2409" data-name="Group 2409" transform="translate(0 0)">
+    <g id="Group_2411" data-name="Group 2411">
+      <path id="Path_2074" data-name="Path 2074" d="M67.027,37.578A18.789,18.789,0,0,1,67.027,0Z" transform="translate(8.129)" fill="#410b5f"/>
+      <path id="Path_2075" data-name="Path 2075" d="M64.318,0a18.789,18.789,0,0,1,0,37.578Z" transform="translate(10.838)" fill="#410b5f"/>
+      <path id="Path_2076" data-name="Path 2076" d="M0,0A18.789,18.789,0,0,1,18.788,18.788,18.789,18.789,0,0,1,0,37.578Z" fill="#410b5f"/>
+      <path id="Path_2077" data-name="Path 2077" d="M34.868,69.737a18.789,18.789,0,0,1,0-37.578Z" transform="translate(2.71 5.419)" fill="#410b5f"/>
+      <path id="Path_2078" data-name="Path 2078" d="M99.186,69.737a18.789,18.789,0,0,1,0-37.578Z" transform="translate(13.548 5.419)" fill="#410b5f"/>
+      <path id="Path_2079" data-name="Path 2079" d="M32.159,32.159a18.789,18.789,0,0,1,0,37.578Z" transform="translate(5.419 5.419)" fill="#410b5f"/>
+    </g>
+  </g>
+  <rect id="Rectangle_284" data-name="Rectangle 284" width="1.247" height="37.423" transform="translate(36.828 37.72)" fill="#410b5f"/>
+  <rect id="Rectangle_285" data-name="Rectangle 285" width="1.247" height="37.008" transform="translate(74.667 0.297)" fill="#410b5f"/>
+</svg>
+        </div> 
+        <div className="container2"> <h3>Office Storage</h3><p className="p1">Lorem Ipsum</p>
+        <svg id="Group_2407" data-name="Group 2407" xmlns="http://www.w3.org/2000/svg" width="15rem" height="12rem" viewBox="0 0 113.249 75.157">
+  <path id="Path_2064" data-name="Path 2064" d="M16.079,18.874a18.875,18.875,0,0,1,37.75,0Z" transform="translate(2.796)" fill="#ff9831"/>
+  <path id="Path_2065" data-name="Path 2065" d="M48.238,18.874a18.875,18.875,0,0,1,37.75,0Z" transform="translate(8.386)" fill="#ff9831"/>
+  <path id="Path_2066" data-name="Path 2066" d="M16.079,34.954a18.875,18.875,0,0,1,37.75,0Z" transform="translate(2.796 2.796)" fill="#410b5f"/>
+  <path id="Path_2067" data-name="Path 2067" d="M48.238,34.954a18.875,18.875,0,0,1,37.75,0Z" transform="translate(8.386 2.796)" fill="#410b5f"/>
+  <path id="Path_2068" data-name="Path 2068" d="M85.988,48.238a18.875,18.875,0,0,1-37.75,0Z" transform="translate(8.387 8.044)" fill="#ff9831"/>
+  <path id="Path_2069" data-name="Path 2069" d="M53.829,48.238a18.875,18.875,0,0,1-37.75,0Z" transform="translate(2.796 8.044)" fill="#ff9831"/>
+  <path id="Path_2070" data-name="Path 2070" d="M85.988,32.159a18.875,18.875,0,0,1-37.75,0Z" transform="translate(8.386 5.249)" fill="#410b5f"/>
+  <path id="Path_2071" data-name="Path 2071" d="M53.829,32.159a18.875,18.875,0,0,1-37.75,0Z" transform="translate(2.796 5.249)" fill="#410b5f"/>
+  <path id="Path_2072" data-name="Path 2072" d="M18.874,53.829a18.875,18.875,0,0,1,0-37.75Z" transform="translate(0 2.453)" fill="#ff9831"/>
+  <path id="Path_2073" data-name="Path 2073" d="M80.4,16.079a18.875,18.875,0,0,1,0,37.75Z" transform="translate(13.977 2.453)" fill="#ff9831"/>
+</svg>
+        </div>
+      </div>
+      <div className="responsive2">
+        <div className="container2"><h3>Locker</h3><p className="p1">Lorem Ipsum</p>
+        <svg id="Group_2412" data-name="Group 2412" xmlns="http://www.w3.org/2000/svg" width="15rem" height="12rem" viewBox="0 0 116.321 77.547">
+  <path id="Path_2080" data-name="Path 2080" d="M64.318,0a19.387,19.387,0,0,1,0,38.774Z" transform="translate(12.797 38.773)" fill="#410b5f"/>
+  <path id="Path_2081" data-name="Path 2081" d="M80.4,0a19.387,19.387,0,0,1,0,38.774Z" transform="translate(16.104 38.773)" fill="#410b5f"/>
+  <path id="Path_2082" data-name="Path 2082" d="M51.545,38.774A19.387,19.387,0,0,1,51.545,0Z" transform="translate(6.615 38.773)" fill="#410b5f"/>
+  <path id="Path_2083" data-name="Path 2083" d="M48.238,0a19.387,19.387,0,0,1,0,38.774Z" transform="translate(9.49 38.773)" fill="#410b5f"/>
+  <rect id="Rectangle_286" data-name="Rectangle 286" width="38.774" height="38.774" transform="translate(0 38.773)" fill="#ff9831"/>
+  <path id="Path_2080-2" data-name="Path 2080" d="M0,0A19.387,19.387,0,0,1,19.386,19.386,19.387,19.387,0,0,1,0,38.774Z" transform="translate(39.206 38.774) rotate(180)" fill="#410b5f"/>
+  <path id="Path_2081-2" data-name="Path 2081" d="M0,0A19.387,19.387,0,0,1,19.386,19.386,19.387,19.387,0,0,1,0,38.774Z" transform="translate(19.82 38.774) rotate(180)" fill="#410b5f"/>
+  <path id="Path_2082-2" data-name="Path 2082" d="M19.386,38.774A19.387,19.387,0,0,1,19.386,0Z" transform="translate(77.548 38.774) rotate(180)" fill="#410b5f"/>
+  <path id="Path_2083-2" data-name="Path 2083" d="M0,0A19.387,19.387,0,0,1,19.386,19.386,19.387,19.387,0,0,1,0,38.774Z" transform="translate(58.593 38.774) rotate(180)" fill="#410b5f"/>
+  <rect id="Rectangle_286-2" data-name="Rectangle 286" width="38.774" height="38.774" transform="translate(116.321 38.774) rotate(180)" fill="#ff9831"/>
+</svg>
+        </div>
+        <div className="container2"> <h3>About Us</h3><p className="p1">Lorem Ipsum</p></div>
+      </div>
+      </div>
     </main>
-  )
+    </body>
+  )  
 }
 
-export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export default IndexPage
